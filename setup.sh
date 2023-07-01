aa22f070aea4379dace52c8e1b4b2605eda1b15a
shaddyjr <shaddyjr@gmail.com>
2023-07-01 05:56:44 -0400

    remove old bash script

--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# pipenv shell
-# start git-bash -c "echo 'Running in window #1'; echo "$PWD"; cd backend/; python manage.py runserver;"
-# start git-bash -c "echo 'Running in window #1'; echo "$PWD"; cd frontend/; npm start;"
